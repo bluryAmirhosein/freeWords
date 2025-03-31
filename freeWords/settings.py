cbf417df7b358f7f0756d24700a7349cf5cebfd8
blue <amirhoseincheraghali21@gmail.com>
2025-04-01 03:28:15 +0330

    js for cropping profile image was fixed.

--- freeWords/settings.py
+++ freeWords/settings.py
@@ -161,11 +161,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> fix/minor-bugs
+# <<<<<<< HEAD
+# =======
+# <<<<<<< Updated upstream
+# =======
+# >>>>>>> fix/minor-bugs
 STORAGES = {
     'default': {
         'BACKEND': 'storages.backends.s3.S3Storage',
@@ -180,23 +180,23 @@ STORAGES = {
 DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 AWS_ACCESS_KEY_ID = '83e887a6-5e0c-4eb1-afca-9ec3a5fcd70f'
 AWS_SECRET_ACCESS_KEY = 'b2f346de4d78543b33b83e3236435106da5c861c7804b87eb38be1113f998e89'
-<<<<<<< HEAD
-AWS_S3_ENDPOINT_URL = 'https://s3.ir-tbz-sh1.arvanstorage.ir'
-AWS_STORAGE_BUCKET_NAME = 'freewordsfiles1234'
-=======
+# <<<<<<< HEAD
+# AWS_S3_ENDPOINT_URL = 'https://s3.ir-tbz-sh1.arvanstorage.ir'
+# AWS_STORAGE_BUCKET_NAME = 'freewordsfiles1234'
+# =======
 AWS_S3_ENDPOINT_URL = 'https://s3.ir-thr-at1.arvanstorage.ir'
 AWS_STORAGE_BUCKET_NAME = 'myfreewordimages1'
->>>>>>> fix/minor-bugs
+# >>>>>>> fix/minor-bugs
 AWS_SERVICE_NAME = 's3'
 AWS_S3_FILE_OVERWRITE = False
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> Stashed changes
->>>>>>> fix/minor-bugs
+# <<<<<<< HEAD
+# =======
+#
+# >>>>>>> Stashed changes
+# >>>>>>> fix/minor-bugs
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
