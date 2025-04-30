7da0c51037a2da7d3d023290a8ea45c6b24681eb
blue <amirhoseincheraghali21@gmail.com>
2025-04-30 11:52:50 +0330

    get ready for public on github

--- staticfiles/account/js/script.js
+++
@@ -1,8 +0,0 @@
-  const inputs = document.querySelectorAll('.w3-input');
-  inputs.forEach(input => {
-    input.addEventListener('input', function () {
-      if (this.classList.contains('input-error')) {
-        this.classList.remove('input-error');
-      }
-    });
-  });
